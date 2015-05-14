40df5ba02134b29831f3cb929ba732123df2dc32
Joshua Scoggins <Theoretically.x64@gmail.com>
2015-05-14 04:33:51 +0000

    Imported c source files for the new lib

--- src/libunicornhat/unicornhat-wrapper.c
+++ src/libunicornhat/unicornhat-wrapper.c
@@ -26,11 +26,76 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #include "clips.h"
 #include "libmaya.h"
-#include "ws2812-RPi.h"
-
 #if UNICORNHAT
+#include "ws2811.h"
+#include "board_info.h"
+
+#define TARGET_FREQ    WS2811_TARGET_FREQ
+#define GPIO_PIN       18
+#define DMA            5
+
+#define WIDTH          8
+#define HEIGHT         8
+#define LED_COUNT      (WIDTH * HEIGHT)
+ws2811_t ledstring =
+{
+    .freq = TARGET_FREQ,
+    .dmanum = DMA,
+    .channel =
+    {
+        [0] =
+        {
+            .gpionum    = GPIO_PIN,
+            .count      = LED_COUNT,
+            .invert     = 0,
+            .brightness = 55,
+        }
+    }
+};
+
+void setBrightness(int b)
+{
+    ledstring.channel[0].brightness = b;
+    return;
+}
+
+void setPixelColorRGB(int pixel, int r, int g, int b)
+{
+    ledstring.channel[0].leds[pixel] = (r << 16) | (g << 8) | b;
+    return;
+}
+
+void clearLEDBuffer(void){
+    int i;
+    for(i=0; i<LED_COUNT;i++){
+        setPixelColorRGB(i,0,0,0);
+    }
+}
+
+/*
+  Remap an x/y coordinate to a pixel index
+*/
+int getPixelPosition(int x, int y){
+
+    int map[8][8] = {
+        {7 ,6 ,5 ,4 ,3 ,2 ,1 ,0 },
+        {8 ,9 ,10,11,12,13,14,15},
+        {23,22,21,20,19,18,17,16},
+        {24,25,26,27,28,29,30,31},
+        {39,38,37,36,35,34,33,32},
+        {40,41,42,43,44,45,46,47},
+        {55,54,53,52,51,50,49,48},
+        {56,57,58,59,60,61,62,63}
+    };
+
+    return map[x][y];
+}
+
+void show(){
+    ws2811_render(&ledstring);
+}
 static void ShowPixels(void* theEnv);
-static double GetBrightness(void* theEnv);
+static int GetBrightness(void* theEnv);
 static int SetBrightness(void* theEnv);
 static int SetPixelColor(void* theEnv);
 static void GetPixelColor(void* theEnv, DATA_OBJECT_PTR ret);
@@ -42,7 +107,7 @@ void UnicornhatInterfaceDefinitions(void* theEnv) {
 	InitializeUnicornhat(theEnv);
 	EnvDefineFunction2(theEnv, "unicornhat:number-of-pixels", 'i', PTIEF NumberOfPixels, "NumberOfPixels", "00a");
 	EnvDefineFunction2(theEnv, "unicornhat:show", 'v', PTIEF ShowPixels, "ShowPixels", "00a");
-	EnvDefineFunction2(theEnv, "unicornhat:get-brightness", 'd', PTIEF GetBrightness, "GetBrightness", "00a");
+	EnvDefineFunction2(theEnv, "unicornhat:get-brightness", 'i', PTIEF GetBrightness, "GetBrightness", "00a");
 	EnvDefineFunction2(theEnv, "unicornhat:set-brightness", 'b', PTIEF SetBrightness, "SetBrightness", "11dd");
 	EnvDefineFunction2(theEnv, "unicornhat:set-pixel-color", 'b', PTIEF SetPixelColor, "SetPixelColor", "44iiiii");
 	EnvDefineFunction2(theEnv, "unicornhat:get-pixel-color", 'u', PTIEF GetPixelColor, "GetPixelColor", "11ii");
@@ -55,30 +120,27 @@ void InitializeUnicornhat(void* theEnv) {
 	show();
 }
 void ShutdownUnicornhat(void* theEnv) {
-	int i;
-
-	for (i = 0; i < 64; i++) {
-		setPixelColor(i,0,0,0);
-	}
-
-	show();
-	terminate(0);
+    int i;
+    for (i = 0; i < 64; i++){
+        setPixelColorRGB(i,0,0,0);
+    }
+    ws2811_render(&ledstring);
+    ws2811_fini(&ledstring);
 }
 int NumberOfPixels(void* theEnv) {
-	return numPixels();
+	return LED_COUNT;
 }
 void ShowPixels(void* theEnv) {
 	show();
-	usleep(10);
 }
 
-double GetBrightness(void* theEnv) {
-	return getBrightness();
+int GetBrightness(void* theEnv) {
+	return ledstring.channel[0].brightness;
 }
 
 int SetBrightness(void* theEnv) {
-	double value;
-	value = EnvRtnDouble(theEnv, 1);
+	long long value;
+	value = EnvRtnLong(theEnv, 1);
 	if (value < 0.0) {
 		EnvPrintRouter(theEnv,WERROR,"Brightness can't be less than zero!\n");
 		return FALSE;
@@ -86,7 +148,7 @@ int SetBrightness(void* theEnv) {
 		EnvPrintRouter(theEnv,WERROR,"Brightness can't be greater than one!\n");
 		return FALSE;
 	} else {
-		return setBrightness(value);
+		setBrightness((unsigned char)value);
 	}
 }
 
@@ -96,7 +158,7 @@ int SetPixelColor(void* theEnv) {
 	unsigned char r, g, b;
    // char* container;
 	pixel = EnvRtnLong(theEnv, 1);
-	if (pixel < 0 || pixel >= numPixels()) {
+	if (pixel < 0 || pixel >= LED_COUNT) {
 		EnvPrintRouter(theEnv,WERROR,"Pixel index is not in range!\n");
 		return FALSE;
 	}
@@ -119,30 +181,31 @@ int SetPixelColor(void* theEnv) {
 	r = (unsigned char)red;
 	g = (unsigned char)green;
 	b = (unsigned char)blue;
-    result = setPixelColor(pixel, r, g, b);
-    return result;
+    setPixelColorRGB(pixel, r, g, b);
+	return TRUE;
 }
 
 void GetPixelColor(void* theEnv, DATA_OBJECT_PTR ret) {
-	Color_t color;
+	ws2811_led_t color;
 	void* multifield;
 	long long index;
 	index = EnvRtnLong(theEnv, 1);
-	if (index < 0 || index >= numPixels()) {
+	if (index < 0 || index >= LED_COUNT) {
 		EnvPrintRouter(theEnv,WERROR,"Pixel index is not in range!\n");
 		SetpType(ret, SYMBOL);
 		SetpValue(ret, EnvFalseSymbol(theEnv));
 		return;
 	}
-	color = getPixelColor(index);
+	color = ledstring.channel[0].leds[index];
 	multifield = EnvCreateMultifield(theEnv, 3);
 
+    //ledstring.channel[0].leds[pixel] = (r << 16) | (g << 8) | b;
 	SetMFType(multifield,1,INTEGER);
-	SetMFValue(multifield, 1, EnvAddLong(theEnv, color.r));
+	SetMFValue(multifield, 1, EnvAddLong(theEnv, (color & 0x00FF0000) >> 16));
 	SetMFType(multifield,2,INTEGER);
-	SetMFValue(multifield, 2, EnvAddLong(theEnv, color.g));
+	SetMFValue(multifield, 2, EnvAddLong(theEnv, (color & 0x0000FF00) >> 8));
 	SetMFType(multifield,3,INTEGER);
-	SetMFValue(multifield, 3, EnvAddLong(theEnv, color.b));
+	SetMFValue(multifield, 3, EnvAddLong(theEnv, (color & 0x000000FF)));
 	SetpType(ret, MULTIFIELD);
 	SetpValue(ret, multifield);
 	SetpDOBegin(ret, 1);
