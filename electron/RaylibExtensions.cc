5acd2e0bf02295ac078488ea308fe4d368cea1c2
Joshua Scoggins <Theoretically.x64@gmail.com>
2025-07-06 16:06:26 -0700

    Add init window and close window to raylib extensions

--- electron/RaylibExtensions.cc
+++ electron/RaylibExtensions.cc
@@ -21,22 +21,57 @@
  * 3. This notice may not be removed or altered from any source distribution.
  *
  */
-
 #include "electron/Environment.h"
 #include "electron/RaylibExtensions.h"
 
 extern "C" {
 #include "clips/clips.h"
 }
+// GetFileName in Raylib conflicts with GetFileName inside of clips!
+#define GetFileName GetFileName_Raylib
+#include <raylib.h>
+#undef GetFileName
 namespace Electron
 {
 
+void raylibInitializeWindow(UDF_ARGS__);
+void raylibCloseWindow(UDF_ARGS__);
 
 void
 InitializeRaylibExtensions(RawEnvironment* env)
 {
     auto& theEnv = Environment::fromRaw(env);
-    //theEnv.addFunction("empty$", "b", 1, 1, "m;m", multifieldEmpty, "multifieldEmpty");
+    theEnv.addFunction("init-window", "b", 3, 3, "syl;l;l;sy", raylibInitializeWindow, "raylibInitializeWindow");
+    theEnv.addFunction("close-window", "b", 0, 0, "*", raylibCloseWindow, "raylibCloseWindow");
+}
+
+void
+raylibInitializeWindow(UDF_ARGS__) {
+    auto& theEnv = Environment::fromRaw(env);
+    UDFValue arg0;
+    UDFValue arg1;
+    UDFValue arg2;
+    if (!theEnv.firstArgument(context, ArgumentBits::Integer, &arg0)) {
+        out->lexemeValue = theEnv.falseSymbol();
+        return;
+    }
+    if (!theEnv.nextArgument(context, ArgumentBits::Integer, &arg1)) {
+        out->lexemeValue = theEnv.falseSymbol();
+        return;
+    }
+    if (!theEnv.nextArgument(context, ArgumentBits::Lexeme, &arg2)) {
+        out->lexemeValue = theEnv.falseSymbol();
+        return;
+    }
+    InitWindow(arg0.integerValue->contents, arg1.integerValue->contents, arg2.lexemeValue->contents);
+    out->lexemeValue = theEnv.trueSymbol();
+}
+
+void
+raylibCloseWindow(UDF_ARGS__) {
+    auto& theEnv = Environment::fromRaw(env);
+    CloseWindow();
+    out->lexemeValue = theEnv.trueSymbol();
 }
 
 } // end namespace Electron
